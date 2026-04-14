9e22559f658ed8c12345fc55a2df09f299b86f73
David <davidalfonsook@gmail.com>
2026-04-13 21:56:02 -0300

    Add many util predicates for dieciseisavos1.

--- fixture.pl
+++ fixture.pl
@@ -1,3 +1,5 @@
+:- use_module(library(random)).
+
 grupoA(mexico).
 grupoA(sudafrica).
 grupoA(corea).
@@ -62,5 +64,24 @@ grupoL(panama).
 
 % predicados utiles
 
-% predicado generico que recibe un predicado por ejemplo grupoA y te devuelve la lista de X para los cuales grupoA(X) es true
+% predicado generico que recibe otro predicado por ejemplo grupoA y te devuelve la lista de argumentos para los cuales grupoA(X) es true
+% si en el interprete hacer por ejempl grupoA(X) te devuelve el primero que encuentra y no sigue el backtracking salvo que se lo especifiques
 lista_de(Predicado, Lista) :- findall(X, call(Predicado, X), Lista).
+
+% predicados que devuelven un elemento particular de una lista
+primero([X|_], X).
+segundo([_,X,_,_], X).
+tercero([_,_,X,_], X).
+cuarto([_,_,_,X], X).
+
+% elige uno entre dos elementos. Simula enfrentamientos directos
+elegir(A, B, X) :- random_member(X, [A,B]).
+
+% random_permutation mezcla las listas para simular que se jugaron partidos
+dieciseisavos1(Ganador) :- lista_de(grupoA, ListaA), 
+                            lista_de(grupoB, ListaB), 
+                            random_permutation(ListaA, GrupoADefinido),
+                            random_permutation(ListaB, GrupoBDefinido),
+                            segundo(GrupoADefinido, SegundoA),
+                            segundo(GrupoBDefinido, SegundoB),
+                            elegir(SegundoA, SegundoB, Ganador).
